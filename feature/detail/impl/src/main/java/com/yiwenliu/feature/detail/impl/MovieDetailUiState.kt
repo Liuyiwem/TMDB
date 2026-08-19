4a49d4691f2c3c6196ad0555691a197d7dadb960
yiwenliu <seed48615@gmail.com>
2026-08-20 04:25:11 +0800

    [Refactor] annotate the ui state holders as immutable

--- feature/detail/impl/src/main/java/com/yiwenliu/feature/detail/impl/MovieDetailUiState.kt
+++ feature/detail/impl/src/main/java/com/yiwenliu/feature/detail/impl/MovieDetailUiState.kt
@@ -1,10 +1,12 @@
 package com.yiwenliu.feature.detail.impl
 
+import androidx.compose.runtime.Immutable
 import com.yiwenliu.core.model.CastMember
 import com.yiwenliu.core.model.Movie
 import com.yiwenliu.core.model.MovieDetail
 import com.yiwenliu.core.ui.util.UiText
 
+@Immutable
 internal data class MovieDetailUiState(
     val isLoading: Boolean = true,
     val detail: MovieDetail? = null,
